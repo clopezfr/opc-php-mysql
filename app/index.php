5feb6d151c2b37856ef3b044529fbd6c9a465066
Christophe <christophe@clopez.fr>
2022-11-24 16:22:34 +0100

    Add custom php.ini to docker

--- app/index.php
+++ app/index.php
@@ -1,4 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Ma page web</title>
+</head>
+<body>
+<h1>Ma page web</h1>
+<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
+</body>
+</html>
+
 <?php
+
+phpinfo();
+
 $host = 'mysql'; //Nom donné dans le docker-compose.yml
 $user = 'root';
 $password = '12345';
